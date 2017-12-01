89b835f2c9fb0e0f06fb086d75a671cff3ab0dfe
luismy83 <luismiguelsaez@geekadmin.es>
2017-12-01 18:38:01 +0100

    Added run.sh

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,5 +1,5 @@
 
 docker run \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $HOME/persistence:/var/lib/jenkins \
+    -v $PWD/persistence:/var/jenkins_home \
     -p 8080:8080 -d --name jenkins-run jenkins-tid
