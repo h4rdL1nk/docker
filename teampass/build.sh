683f75e90f694c07ebf4b3232d3f5e995de0c20a
h4rdL1nk <imthedanger@protonmail.com>
2018-01-29 10:57:58 +0100

    Fixed container start order

--- teampass/build.sh
+++ teampass/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sudo docker build -t teampass-app -f Dockerfile.web .
-sudo docker run -d --name teampass-app-run --link teampass-db-run:db -p80:80 -p443:443 teampass-app
 sudo docker run -d --name teampass-db-run -p3306:3306 -e MYSQL_ROOT_PASSWORD=4dm1nSd1g1tS -e MYSQL_DATABASE=teampass -e MYSQL_PASSWORD=t34mP4sSw0rD -e MYSQL_USER=teampassApp  mysql:5.6
-
+sudo docker run -d --name teampass-app-run --link teampass-db-run:db -p80:80 -p443:443 teampass-app
