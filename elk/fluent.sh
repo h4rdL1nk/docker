224639e11b5bade28aa1ffba91ff845371eeac65
h4rdL1nk <imthedanger@protonmail.com>
2018-03-20 15:45:04 +0100

    updated

--- elk/fluent.sh
+++ elk/fluent.sh
@@ -1,1 +1,2 @@
-docker run -d --name log-shipper -v /var/lib/docker:/data:ro -v $PWD/conf/fluentd.conf:/fluentd/etc/fluentd-alt.conf:ro -e FLUENTD_CONF=fluentd-alt.conf -e FLUENTD_OPT="-vv" -e FLUENT_UID=1 fluent/fluentd
+docker build -t fluentd-elastic dockerfiles/fluentd
+docker run -d --network elk_es-cl0-net --name log-shipper -v /var/lib/docker:/data:ro -v $PWD/conf/fluentd.conf:/fluentd/etc/fluentd-alt.conf:ro -e FLUENTD_CONF=fluentd-alt.conf -e FLUENT_UID=1 fluentd-elastic
