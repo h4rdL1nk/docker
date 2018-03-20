2281f8da12a0820d233f7e7038389d02182cec96
h4rdL1nk <imthedanger@protonmail.com>
2018-03-20 10:12:09 +0100

    Added fluentd container launch commando

---
+++ elk/fluent.sh
@@ -0,0 +1,1 @@
+docker run -d --name log-shipper -v /var/lib/docker:/data:ro -v $PWD/conf/fluentd.conf:/fluentd/etc/fluentd-alt.conf:ro -e FLUENTD_CONF=fluentd-alt.conf -e FLUENTD_OPT="-vv" -e FLUENT_UID=1 fluent/fluentd
