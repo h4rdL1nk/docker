3389df287e29a7f462e7df7befb152159c2f044f
luismy83 <luismiguelsaez@geekadmin.es>
2018-03-26 15:17:19 +0200

    Added config file example for RHEL

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,6 +1,5 @@
 
 docker run \
-    -e JAVA_OPTS="-Xm512m -Dcom.sun.jndi.ldap.connect.pool.protocol=DIGEST-MD5 -Dorg.apache.commons.jelly.tags.fmt.timeZone=Europe/Madrid" \
+    -e JAVA_OPTS="-Xmx512m -Dcom.sun.jndi.ldap.connect.pool.protocol=DIGEST-MD5 -Dorg.apache.commons.jelly.tags.fmt.timeZone=Europe/Madrid" \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $PWD/persistence:/var/jenkins_home \
-    -p 8080:8080 -d --name jenkins-run jenkins
+    -p 8080:8080 -d --name jenkins-run jenkins:alpine
