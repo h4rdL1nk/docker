dc5c97d2165bd8568b9832cf8cae1a91d8580265
h4rdL1nk <imthedanger@protonmail.com>
2018-04-09 16:41:40 +0200

    Added connectivity tools

---
+++ jenkins/git/gitconfig.sh
@@ -0,0 +1,4 @@
+git config --global http.sslVerify false
+git config --global https.sslVerify false
+git config --global http.proxy http://proxy2pdi.service.dsn.inet:6666
+git config --global https.proxy http://proxy2pdi.service.dsn.inet:6666
